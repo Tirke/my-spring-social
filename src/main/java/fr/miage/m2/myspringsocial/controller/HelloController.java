bef2963479ea303818327313f8bbdd726409d48d
Thomas Schersach <toj315@gmail.com>
2017-12-28 00:17:21 +0100

    Twitter sample

--- src/main/java/fr/miage/m2/myspringsocial/controller/HelloController.java
+++ src/main/java/fr/miage/m2/myspringsocial/controller/HelloController.java
@@ -4,6 +4,8 @@ package fr.miage.m2.myspringsocial.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.linkedin.api.LinkedIn;
+import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,40 +14,53 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 
   private Facebook facebook;
+  private Twitter twitter;
+  private LinkedIn linkedIn;
   private ConnectionRepository connectionRepo;
 
   @Autowired
   public HelloController(Facebook facebook,
+      Twitter twitter, LinkedIn linkedIn,
       ConnectionRepository connectionRepo) {
     this.facebook = facebook;
+    this.twitter = twitter;
+    this.linkedIn = linkedIn;
     this.connectionRepo = connectionRepo;
   }
 
-  @GetMapping("/")
-  public String fetchData(Model model) {
+  @GetMapping("/twitter")
+  public String helloTwitter(Model model) {
+    if (connectionRepo.findPrimaryConnection(Twitter.class) == null) {
+      return "redirect:/connect/twitter";
+    }
+
+    System.out.println(twitter.timelineOperations().getUserTimeline());
+    // Renvoi 20 tweets max
+    System.out.println(twitter.timelineOperations().getHomeTimeline());
+    // L'appel de méthode avec la signature int,long,long permet de parcourir l'ensemble.
+    // https://developer.twitter.com/en/docs/tweets/timelines/guides/working-with-timelines
+    // J'utilise le home feed pr le moment car j'ai un compte tweeter tout neuf.
+    System.out.println(twitter.timelineOperations().getHomeTimeline());
+
+    twitter.timelineOperations().getHomeTimeline().forEach(t -> {
+      System.out.println(t.getId() + " tweet id");
+      System.out.println(t.getText() + " tweet text");
+      System.out.println(t.getIdStr() + " tweet id string");
+      System.out.println(t.getSource() + " tweet id source");
+    });
+
+    return "index";
+  }
+
+  @GetMapping("/fb")
+  public String helloFb(Model model) {
 
     if (connectionRepo.findPrimaryConnection(Facebook.class) == null) {
       return "redirect:/connect/facebook";
     }
 
-//    System.out.println(facebook.userOperations().getUserProfile());
-//    System.out.println(facebook.feedOperations().getPosts());
-//    System.out.println(facebook.feedOperations().getHomeFeed());
-//    System.out.println(facebook.feedOperations().getFeed());
+
     model.addAttribute("profile", facebook.userOperations().getUserProfile());
-//    model.addAttribute("feed", facebook.feedOperations().getFeed());
     return "index";
   }
-
-//  @GetMapping("/FBcb")
-//  public String wtf(@RequestParam(value = "code") String oauthCode, HttpSession session) throws FacebookException {
-//    Facebook facebook = (Facebook) session.getAttribute("facebook");
-//    AccessToken token = facebook.getOAuthAccessToken(oauthCode);
-//    System.out.println(token);
-//    System.out.println(facebook.permissions().getPermissions());
-//    System.out.println(facebook.getHome());
-//    System.out.println("im in cb");
-//    return "index";
-//  }
-
 }
