1ed837b4cf5be5ff1e8484a151ad18cd07ed0bcd
Thomas Schersach <toj315@gmail.com>
2017-12-27 22:01:51 +0100

    Replaced facebook4j example with spring social example

--- src/main/java/fr/miage/m2/myspringsocial/controller/HelloController.java
+++ src/main/java/fr/miage/m2/myspringsocial/controller/HelloController.java
@@ -1,52 +1,51 @@
 package fr.miage.m2.myspringsocial.controller;
 
-import facebook4j.Facebook;
-import facebook4j.FacebookException;
-import facebook4j.FacebookFactory;
-import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HelloController {
 
-//  private Facebook facebook;
-//  private ConnectionRepository connectionRepo;
-//
-//  @Autowired
-//  public HelloController(Facebook facebook,
-//      ConnectionRepository connectionRepo) {
-//    this.facebook = facebook;
-//    this.connectionRepo = connectionRepo;
-//  }
+  private Facebook facebook;
+  private ConnectionRepository connectionRepo;
+
+  @Autowired
+  public HelloController(Facebook facebook,
+      ConnectionRepository connectionRepo) {
+    this.facebook = facebook;
+    this.connectionRepo = connectionRepo;
+  }
 
   @GetMapping("/")
-  public RedirectView fetchData(HttpSession session) throws FacebookException {
-    Facebook facebook = new FacebookFactory().getInstance();
-    session.setAttribute("facebook", facebook);
-    RedirectView redirectView = new RedirectView();
-    redirectView.setUrl(facebook.getOAuthAuthorizationURL("http://localhost:8080/FBcb"));
-    return redirectView;
-//    ResponseList<Post> feed = facebook.getHome();
-//    System.out.println(feed);
+  public String fetchData(Model model) {
 
-//    if (connectionRepo.findPrimaryConnection(Facebook.class) == null) {
-//      return "redirect:/connect/facebook";
-//    }
-//
-//    model.addAttribute("profile", facebook.userOperations().getUserProfile());
-////    model.addAttribute("feed", facebook.feedOperations().getFeed());
-//    return "index";
-  }
+    if (connectionRepo.findPrimaryConnection(Facebook.class) == null) {
+      return "redirect:/connect/facebook";
+    }
 
-  @GetMapping("/FBcb")
-  public String wtf(HttpSession session) throws FacebookException {
-    Facebook facebook = (Facebook) session.getAttribute("facebook");
-    System.out.println(facebook.permissions().getPermissions());
-    System.out.println(facebook.getHome());
-    System.out.println("im in cb");
+//    System.out.println(facebook.userOperations().getUserProfile());
+//    System.out.println(facebook.feedOperations().getPosts());
+//    System.out.println(facebook.feedOperations().getHomeFeed());
+//    System.out.println(facebook.feedOperations().getFeed());
+    model.addAttribute("profile", facebook.userOperations().getUserProfile());
+//    model.addAttribute("feed", facebook.feedOperations().getFeed());
     return "index";
   }
+
+//  @GetMapping("/FBcb")
+//  public String wtf(@RequestParam(value = "code") String oauthCode, HttpSession session) throws FacebookException {
+//    Facebook facebook = (Facebook) session.getAttribute("facebook");
+//    AccessToken token = facebook.getOAuthAccessToken(oauthCode);
+//    System.out.println(token);
+//    System.out.println(facebook.permissions().getPermissions());
+//    System.out.println(facebook.getHome());
+//    System.out.println("im in cb");
+//    return "index";
+//  }
 
 }
