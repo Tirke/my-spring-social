e773105fc1044d552c91a40ab3e3f5ac6fc347a2
Thomas Schersach <toj315@gmail.com>
2018-01-13 14:32:53 +0100

    Major improvements to profiles

--- src/main/java/fr/miage/m2/myspringsocial/fb/FacebookController.java
+++ src/main/java/fr/miage/m2/myspringsocial/fb/FacebookController.java
@@ -1,8 +1,12 @@
 package fr.miage.m2.myspringsocial.fb;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,9 +31,19 @@ public class FacebookController {
       return "redirect:/connect/facebook";
     }
 
-    model.addAttribute("profile", facebook.userOperations().getUserProfile());
+    Integer totalFriends = facebook.friendOperations().getFriendIds().getTotalCount();
+    User profile = facebook.userOperations().getUserProfile();
+    model.addAttribute("profile", profile);
+    model.addAttribute("age", getUserAge(profile));
+    model.addAttribute("totalFriends", totalFriends);
 
     return "facebook/profile";
+  }
+
+  private int getUserAge(User profile) {
+    LocalDate birthday = LocalDate.parse(profile.getBirthday(),
+        DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+    return Period.between(birthday, LocalDate.now()).getYears();
   }
 
 
