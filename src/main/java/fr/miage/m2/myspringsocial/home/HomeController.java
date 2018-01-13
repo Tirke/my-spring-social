60e6708f1d331a17bb79da749bd05d8a82c99313
EL TAHHAN Sana <sana.eltahhan@hotmail.fr>
2018-01-13 01:10:23 +0100

    Start front home page

--- src/main/java/fr/miage/m2/myspringsocial/home/HomeController.java
+++ src/main/java/fr/miage/m2/myspringsocial/home/HomeController.java
@@ -2,7 +2,11 @@ package fr.miage.m2.myspringsocial.home;
 
 import fr.miage.m2.myspringsocial.account.AccountDetails;
 import fr.miage.m2.myspringsocial.config.CurrentUser;
+import fr.miage.m2.myspringsocial.event.Event;
+import fr.miage.m2.myspringsocial.event.EventRepository;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +15,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class HomeController {
 
+
+  @Autowired
+  EventRepository er;
+
+
   @GetMapping("/")
   public String home(@CurrentUser AccountDetails user, Model model) {
     // CurrentUser is a way to retrieve the current user , null when anonymous
+    List<Event> event = er.findAll();
+    model.addAttribute("events", event);
     return "index";
   }
+
 
 }
