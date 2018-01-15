7af4d20a212722a05909256c32f26c5b8f4a1b21
Anne Jacquet <anne-jacquet@sfr.fr>
2018-01-15 21:33:55 +0100

    we can get json events

--- src/main/java/fr/miage/m2/myspringsocial/home/HomeController.java
+++ src/main/java/fr/miage/m2/myspringsocial/home/HomeController.java
@@ -1,5 +1,7 @@
 package fr.miage.m2.myspringsocial.home;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import fr.miage.m2.myspringsocial.account.AccountDetails;
 import fr.miage.m2.myspringsocial.config.CurrentUser;
 import fr.miage.m2.myspringsocial.event.Event;
@@ -7,10 +9,16 @@ import fr.miage.m2.myspringsocial.event.EventRepository;
 import fr.miage.m2.myspringsocial.event.EventType;
 import fr.miage.m2.myspringsocial.social.fb.FacebookService;
 import fr.miage.m2.myspringsocial.social.twitter.TwitterService;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -51,6 +59,26 @@ public class HomeController {
     facebookService.fetchRecent(user);
     twitterService.fetchRecent(user);
     return "redirect:";
+  }
+
+  @GetMapping(value = "/download", produces = "application/json")
+  public ResponseEntity<Resource> download(@CurrentUser AccountDetails user,
+      HttpServletResponse response) throws IOException {
+    List<EventType> eventTypes = new ArrayList<>();
+    eventTypes.add(EventType.SHARED_BY);
+    eventTypes.add(EventType.LIKED_BY);
+    eventTypes.add(EventType.COMMENTED_BY);
+    List<Event> events = er.getEventsForUser(user.getUserId(), eventTypes);
+    GsonBuilder builder = new GsonBuilder();
+    Gson gson = builder.create();
+    String json = gson.toJson(events);
+
+    ByteArrayResource resource = new ByteArrayResource(json.getBytes());
+    return ResponseEntity.ok()
+        .header("Content-Disposition", "attachment; filename=events.json")
+        .contentLength(resource.contentLength())
+        .contentType(MediaType.parseMediaType("application/octet-stream"))
+        .body(resource);
   }
 
 
